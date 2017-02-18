eb2c9eb0aded771f33e7d8e2b66eea954234e1c7
Dimon <Dimon>
2017-02-18 17:35:40 +0200

    Initial commit

---
+++ src/First.java
@@ -0,0 +1,9 @@
+public class First {
+    public static void main(String[] args) {
+        int chislo = 777;
+        if (chislo > 0)
+            System.out.println(chislo + 1);
+        else
+            System.out.println(chislo);
+    }
+}
