d8e7cb76486394e2ecab5d2de08b6f99a61b8221
BaltazarBlade <BaltazarBlade@email.ua>
2017-02-19 12:19:37 +0200

    Add files via upload

---
+++ src/Second.java
@@ -0,0 +1,16 @@
+public class Second {
+
+    public static void main(String[] args) {
+        int chislo2 = 0;
+
+        if (chislo2 > 0){
+            System.out.println(chislo2 + 1);
+        } else if (chislo2 < 0){
+            System.out.println(chislo2 - 2);
+        } else if (chislo2 == 0){
+            System.out.println(10);
+        } else {
+            System.out.println("Error");
+        }
+    }
+}
