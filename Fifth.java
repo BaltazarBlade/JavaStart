ed7f1acfaf620176b8e83459848f21d36c565bc9
BaltazarBlade <BaltazarBlade@email.ua>
2017-02-21 14:01:17 +0200

    Add files via upload

---
+++ Fifth.java
@@ -0,0 +1,6 @@
+public class Fifth {
+    public static void main(String[] args) {
+        int[] stroka = {'1','2','3','4','5'};
+        System.out.println("Количество цифр в массиве stroka; " + stroka.length);
+    }
+}
