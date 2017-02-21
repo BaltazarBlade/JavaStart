586d4f70b154dd509eb5c8aa1c9f24136dcda1fa
BaltazarBlade <BaltazarBlade@email.ua>
2017-02-21 14:01:58 +0200

    Delete Fifth.java

--- Fifth.java
+++
@@ -1,6 +0,0 @@
-public class Fifth {
-    public static void main(String[] args) {
-        int[] stroka = {'1','2','3','4','5'};
-        System.out.println("Количество цифр в массиве stroka; " + stroka.length);
-    }
-}
