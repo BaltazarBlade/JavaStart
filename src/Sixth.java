0c831f294fa5d60f2c9b8708f4b095f1f0c6a316
BaltazarBlade <BaltazarBlade@email.ua>
2017-02-22 14:52:47 +0200

    Add files via upload

---
+++ src/Sixth.java
@@ -0,0 +1,6 @@
+public class Sixth {
+    public static void main(String[] args) {
+        String stroka = "A b d f E G";
+        System.out.println(stroka.replaceAll("b d f" , "B D F"));
+    }
+}
